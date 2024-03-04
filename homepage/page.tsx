d0fa9bf0fdbad6f55c09f9ecd1966522d059efb1
Mohanraj P <124885154+Mohanraj-Me@users.noreply.github.com>
2024-03-04 23:19:49 +0530

    Add files via upload

---
+++ homepage/page.tsx
@@ -0,0 +1,223 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button, Accordion, Card, Spinner, Modal } from "react-bootstrap";
+import Styles from "./page.module.css";
+import baffle from "baffle";
+import Link from "next/link";
+import HoverCarousel from "hover-carousel";
+import Image from "next/image";
+
+const HomePage = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const images = [
+    "UX-Course-1.jpg",
+    "UX-Course-2.jpg",
+    "UX-Course-3.jpg",
+    "UX-Course-4.jpg",
+    "UX-Course-5.jpg",
+    "UX-Course-6.jpg",
+    "UX-Course-7.jpg",
+  ];
+
+  const [showModal, setShowModal] = useState(false);
+  useEffect(() => {
+    const designs = document.querySelectorAll(`.${Styles.design}`);
+
+    designs.forEach((design) => {
+      const text = baffle(design);
+      text.set({
+        characters: "IQLWXZY",
+        speed: 100,
+      });
+
+      text.start();
+      text.reveal(100000);
+    });
+
+    const isMobileOrTablet =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    if (isMobileOrTablet) {
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2000);
+    }
+  }, []);
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleDownload = () => {
+    const resumeUrl = "./other/Mohanraj_CV.pdf";
+    window.open(resumeUrl, "_blank");
+  };
+
+  const handleButtonClick = () => {
+    window.open("https://mohanraj130298.wixstudio.io/portfolio", "_blank");
+  };
+
+  return (
+    <div className={Styles.body}>
+      <div
+        className={`${Styles.backtest}  d-flex align-items-center justify-content-center`}
+      >
+        <span className={`${Styles.showcaseee}`}>I&apos;M</span>
+      </div>
+      <div>
+        <div
+          className={`${Styles.hero} p-3 d-flex align-items-start justify-content-center flex-column`}
+        >
+          <span className={Styles.design}>MOHANRAJ</span>
+          <span className={Styles.design}>DESIGN</span>
+          <span className={Styles.design}>ENTHUSIAST</span>
+          <div className="d-flex d-inline-block">
+            <Button
+              variant="outline-light"
+              onClick={handleDownload}
+              className={`${Styles.button}`}
+            >
+              DOWNLOAD CV
+            </Button>
+          </div>
+        </div>
+
+        <div
+          className={`${Styles.heroTwo}  p-3 d-flex align-items-center justify-content-center text-center flex-column`}
+        >
+          <span className={Styles.para}>
+            I thrive on collaborating with creative teams and pushing the
+            boundaries of what&apos;s possible in the design world. Whether
+            it&apos;s crafting captivating graphics, crafting user-friendly web
+            experiences, sculpting innovative products, weaving fashion
+            statements, or diving into any other medium, I&apos;m always excited
+            to embark on new journeys and stretch the limits of my imagination.
+          </span>
+        </div>
+
+        <div className={`${Styles.slideover}`}></div>
+
+        <div className={`${Styles.showcase} d-flex flex-column`}>
+          <div
+            className={`${Styles.showcasein} d-flex align-items-center justify-content-center`}
+          >
+            <Link href="/developer">
+              <Button className={Styles.dev}>DEVELOPER</Button>
+            </Link>
+          </div>
+          <div className={`${Styles.twoCon}`}>
+            <div
+              className={`${Styles.showcaseout} d-flex align-items-center justify-content-center`}
+            >
+              <Link href="/uidesigner">
+                <Button className={Styles.devui}>UI UX</Button>
+              </Link>
+            </div>
+            <div
+              className={`${Styles.showcaseoutin} d-flex align-items-center justify-content-center`}
+            >
+              <Link href="/designer">
+                <Button
+                  className={Styles.devui}
+                  style={{ textDecoration: "underline" }}
+                >
+                  &MORE
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* <div className="bg-dark ">
+          <span
+            className={`${Styles.showcasee} d-flex align-item-center justify-content-center`}
+          >
+            CERTIFICATION
+          </span>
+          <HoverCarousel images={images} />
+        </div> */}
+
+        <div>
+          <div className={`${Styles.back}`}>
+            <div className={`${Styles.scroll}`}>
+              <span>EXPLORE MY EARLY DESIGN</span>
+            </div>
+          </div>
+          <div className="d-flex align-items-center justify-content-start">
+            <div className={`${Styles.in} d-flex flex-column `}>
+              <span className={Styles.intotext}>
+                &quot;Step into the past! Explore my early design crafted on
+                Wix—a showcase of customized creativity.&quot;
+              </span>
+              <span className={`${Styles.intotext}`}>
+                Click the button for a glimpse of my old portfolio version.
+              </span>
+              <Button
+                variant="dark"
+                className={`${Styles.butto} px-5 mt-4 `}
+                onClick={handleButtonClick}
+              >
+                <span className="pe-2">Visit old version</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.854 10.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707z"
+                  />
+                </svg>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal
+        show={showModal}
+        backdrop="static"
+        keyboard={false}
+        onHide={handleCloseModal}
+      >
+        <Modal.Body>
+          <div className="d-flex flex-column" style={{ fontSize: "20px" }}>
+            Switch to desktop for best view.
+            <Button variant="dark" className="mt-3" onClick={handleCloseModal}>
+              Okay, got it
+            </Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+      <Modal
+        dialogClassName={Styles.showModal}
+        show={show}
+        onHide={handleClose}
+        centered
+      >
+        <Modal.Body>
+          <iframe
+            src="/other/Mohanraj_CV.pdf"
+            style={{ width: "100%", height: "80vh" }}
+          />
+          <Button
+            variant="dark"
+            className="mt-3 float-end"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ uidonation/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import React, { useState } from "react";
+import Styles from "./page.module.css";
+import { Button, Modal, Carousel } from "react-bootstrap";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const Uidonation = () => {
+  const router = useRouter();
+  const handleOpenFigma = () => {
+    window.open(
+      "https://www.figma.com/proto/krFFajeKrkTvKef0s0HRZ7/Donation-Webdesign?type=design&node-id=1-2&t=PUR8JAYKcoTo8bHl-1&scaling=min-zoom&page-id=0%3A1&starting-point-node-id=1%3A2&show-proto-sidebar=1&mode=design",
+      "_blank"
+    );
+  };
+
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const goBack = () => {
+    router.back();
+  };
+
+  return (
+    <div className="">
+      <div className={`${Styles.freeze} text-center`}>
+        <span className={`${Styles.title}`}>DONATION FLOW UI</span>
+      </div>
+
+      <div>
+        <div className={`${Styles.buttonadjust} d-flex justify-content-center`}>
+          <Link href="./" className="nav-link">
+            <Button
+              className={`${Styles.buttonback} d-flex align-items-center px-3
+        `}
+              variant="outline-dark"
+              onClick={goBack}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+                />
+              </svg>
+              <span className="ms-2">Back</span>
+            </Button>
+          </Link>
+          <Button
+            className={`${Styles.buttonback} d-flex align-items-center ms-3 px-3
+        `}
+            variant="dark"
+            onClick={handleOpenFigma}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.854 10.803a.5.5 0 1 1-.708-.707L9.243 6H6.475a.5.5 0 1 1 0-1h3.975a.5.5 0 0 1 .5.5v3.975a.5.5 0 1 1-1 0V6.707z"
+              />
+            </svg>
+            <span className="ms-2">Open in Figma </span>
+          </Button>
+        </div>
+        <div className={`${Styles.work} d-flex justify-content-center`}>
+          <Image
+            src="./other/UI1.png"
+            alt="ui design project"
+            width={1334}
+            height={4166}
+          />
+        </div>
+        <div className="d-flex justify-content-center align-items-center mb-5">
+          <Button
+            className={`${Styles.buttonback} d-flex align-items-center px-5
+        `}
+            variant="dark"
+            onClick={handleScrollToTop}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"
+              />
+            </svg>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Uidonation;
